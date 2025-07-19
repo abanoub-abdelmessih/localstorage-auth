a3c1d6c5047bd4f21a148488b8c8470830cbecba
Abanoub-Abd-Elmessih <abanoubabdelmessih110@gmail.com>
2025-07-19 20:44:24 +0300

    add register page

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+var registerForm = document.getElementById("registerForm");
+var localStorageUsers = JSON.parse(localStorage.getItem("users")) || [];
+
+if (registerForm) {
+  registerForm.addEventListener("submit", registration);
+}
+function registration(e) {
+  e.preventDefault();
+
+  // get registration inputs
+  var nameInput = document.getElementById("registerUsername");
+  var emailInput = document.getElementById("registerEmail");
+  var passwordInput = document.getElementById("registerPassword");
+
+  // to rest inputs after registration
+  var name = nameInput.value.trim();
+  var email = emailInput.value.trim();
+  var password = passwordInput.value.trim();
+
+  if (!name || !email | password) {
+    alert("enter valid data");
+    return;
+  }
+
+  //  to check if the email is already existed
+  var existingEmail = false;
+  for (var i = 0; i < localStorageUsers.length; i++) {
+    if (localStorageUsers[i].email === email) {
+      existingEmail = true;
+      break;
+    }
+  }
+
+  if (existingEmail) {
+    alert("this email is already registered please login");
+    window.location.href = "index.html";
+    return;
+  }
+
+  //   add the new user to the array then save it in localstorage
+  var user = { name, email, password };
+  localStorageUsers.push(user);
+  localStorage.setItem("users", JSON.stringify(localStorageUsers));
+  alert("Registration successful you will be redirected to the login page");
+
+  //   reset inputs
+  nameInput.value = "";
+  emailInput.value = "";
+  passwordInput.value = "";
+
+  window.location.href = "index.html";
+}
